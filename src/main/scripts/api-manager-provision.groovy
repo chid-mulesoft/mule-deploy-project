a38da349213f3ebf478f7843bee3ab87e988a895
Derek Lin <linxg23@gmail.com>
2018-05-18 11:27:18 +1000

    add the logic for provisioning in API Manager

--- src/main/scripts/api-manager-provision.groovy
+++ src/main/scripts/api-manager-provision.groovy
@@ -1,29 +1,284 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput 
+import groovyx.net.http.*
+import groovyx.net.http.ContentType.*
+import groovyx.net.http.Method.*
 
-class CrowdCICDUtil
+
+class CICDUtil
 {
+	static def int WARN=1;
+	static def int INFO=2;
+	static def int DEBUG=3;
+	static def int TRACE=4;
 
-	static def extractExchangeDetail()
+
+
+	static def logLevel = DEBUG;  //root logger level
+
+	static def log (java.lang.Integer level, java.lang.Object content)
 	{
-		println "START extractExchangeDetail "
+		if (level <= logLevel)
+		{
+			def logPrefix = new Date().format("YYYYMMdd-HH:mm:ss") 
+			if (level == WARN)
+			{
+				logPrefix += " WARN"
+			}
+			if (level == INFO)
+			{
+				logPrefix += " INFO"
+			}
+			if (level == DEBUG)
+			{
+				logPrefix += " DEBUG"
+			}
+			if (level == TRACE)
+			{
+				logPrefix += " TRACE"
+			}
+			println logPrefix + " : " + content 
+		}
 
-		def inputFile = new File("crowd/classes/exchange.json")
+	}
+	def extractExchangeAssetDetail(props)
+	{
+		log(DEBUG,  "START extractExchangeAssetDetail")
+
+		def inputFile = new File(props.exchangeFileName)
 		def exchangeDetail = new JsonSlurper().parseText(inputFile.text)
-		//exchangeDetail.each{ println it }
+
+
 		def apiVersion=exchangeDetail.apiVersion
 		def assetId=exchangeDetail.assetId
 		def groupId=exchangeDetail.groupId
 		def assetVersion=exchangeDetail.version
 
+		exchangeDetail.each{ log(INFO, it) }
+		
+		log(DEBUG,  "END extractExchangeAssetDetail")
 
-		println "apiVersion=" + apiVersion
-		println "assetId=" + assetId
-		println "groupId=" + groupId
-		println "assetVersion=" + assetVersion
-		println "END extractExchangeDetail "
+		return exchangeDetail;
+	}
+
+	def getAnypointToken(props)
+	{
+		log(DEBUG,  "START getAnypointToken")
+
+
+		def username=props.username
+		def password=props.password 
+
+
+		log(TRACE, "username=" + username)
+		log(TRACE, "password=" + password)
+
+		def urlString = "https://anypoint.mulesoft.com/accounts/login"
+
+		def message = 'username='+username+'&password='+password
+
+		def headers=["Content-Type":"application/x-www-form-urlencoded", "Accept": "application/json"]
+
+		def connection = doRESTHTTPCall(urlString, "POST", message, headers)
+
+		if ( connection.responseCode =~ '2..') 
+		{
+
+		}else
+		{
+			throw new Exception("Failed to get the login token!")
+		}
+
+		def response = "${connection.content}"
+
+		def token = new JsonSlurper().parseText(response).access_token
+
+		log(INFO, "Bearer Token: ${token}")
+
+		log(DEBUG,  "END getAnypointToken")
+
+		return token
+
 	}
 	
+	def init ()
+	{
+		def props = ['username':System.properties.'username', 
+					 'password': System.properties.'password',
+					 'exchangeFileName': System.properties.'exchangeFileName',
+					 'orgId': System.properties.'orgId',
+					 'envId': System.properties.'envId'
+					]
+
+		return props;
+	}
+
+
+	def provisionAPIManager(props, exchangeDetail)
+	{
+		def token = getAnypointToken(props);
+
+		def result = getAPIInstanceByExchangeAssetDetail(props, exchangeDetail, token);
+
+		log(INFO, "apiInstance=" + result)
+
+
+	}
+
+	def getAPIInstanceByExchangeAssetDetail(props, exchangeDetail, token)
+	{
+
+		log(DEBUG,  "START getAPIInstanceByExchangeAssetDetail")
+
+		def apiInstance
+		def apiDiscoveryName
+		def apiDiscoveryVersion
+
+		def urlString = "https://anypoint.mulesoft.com/exchange/api/v1/assets/"+props.orgId+"/"+exchangeDetail.assetId
+
+		def headers=["Content-Type":"application/json", "Authorization": "Bearer " + token, "Accept": "application/json"]
+
+		def connection = doRESTHTTPCall(urlString, "GET", null, headers)
+
+		if (connection.responseCode == 404)
+		{
+			log(INFO, "API Instance for " + exchangeDetail.assetId + " is not found in API Manager")
+
+		} 
+		else if (connection.responseCode == 200)
+		{
+			log(INFO, "API Instances for " + exchangeDetail.assetId + " has been found in the platform ");
+
+			def response = "${connection.content}"
+
+			def allAPIInstances = new JsonSlurper().parseText(response).instances;
+
+			allAPIInstances.each{ 
+				log(INFO, it)
+				if (it.environmentId == props.envId)
+				{
+					apiInstance = it;
+					apiDiscoveryName = "groupId:"+props.orgId+":assetId:"+ exchangeDetail.assetId
+					apiDiscoveryVersion = apiInstance.name
+				}
+			}
+
+			log(INFO, "apiInstance for env " + props.envId + " is " + apiInstance);
+
+		}
+
+		if (apiInstance == null)
+		{
+			apiInstance = createAPIInstance(token, exchangeDetail, props)
+			apiDiscoveryName = "groupId:"+props.orgId+":assetId:"+ exchangeDetail.assetId
+			apiDiscoveryVersion = apiInstance.autodiscoveryInstanceName
+
+		}
+
+		def result = ["apiInstance": apiInstance, "apiDiscoveryName": apiDiscoveryName, "apiDiscoveryVersion":apiDiscoveryVersion]
+
+		log(DEBUG,  "END getAPIInstanceByExchangeAssetDetail")
+
+		return result
+
+	}
+
+	def createAPIInstance(token, exchangeDetail, props)
+	{
+		log(DEBUG,  "START createAPIInstance")
+
+		def requestTemplate = '{ "endpoint": { "deploymentType": null, "isCloudHub": null, "muleVersion4OrAbove": null, "proxyUri": null, "referencesUserDomain": null, "responseTimeout": null, "type": null, "uri": null }, "instanceLabel": null, "spec": { "assetId": null, "groupId": null, "version": null } }'
+
+		def request = new JsonSlurper().parseText(requestTemplate);
+
+		request.endpoint.deploymentType = 'CH'
+		request.endpoint.type='rest-api'
+		request.spec.assetId=exchangeDetail.assetId
+		request.spec.groupId=props.orgId
+		request.spec.version=exchangeDetail.version
+
+		def message = JsonOutput.toJson(request)
+		log(INFO, "createAPIInstance request message=" + message);
+
+		def urlString = "https://anypoint.mulesoft.com/apimanager/api/v1/organizations/"+props.orgId+"/environments/"+props.envId + "/apis"
+
+		def headers=["Content-Type":"application/json", "Authorization": "Bearer " + token, "Accept": "application/json"]
+
+		def connection = doRESTHTTPCall(urlString, "POST", message, headers)
+
+		def response = "${connection.content}"
+
+		if ( connection.responseCode =~ '2..') 
+		{
+			log(INFO, "the API instance is created successfully! statusCode=" + connection.responseCode)
+		}
+		else
+		{
+			throw new Exception("Failed to create API Instance! statusCode=${connection.responseCode} responseMessage=${response}")
+		}
+	
+
+		def apiInstance = new JsonSlurper().parseText(response)
+
+		log(DEBUG,  "END createAPIInstance")
+
+		return apiInstance;
+	}
+
+	static def doRESTHTTPCall(urlString, method, payload, headers)
+	{
+		log(DEBUG,  "START doRESTHTTPCall")
+
+		log(INFO, "requestURl is " + urlString)
+
+		def url = new URL(urlString)
+
+		def connection = url.openConnection()
+
+		headers.keySet().each {
+			log(INFO, it + "->" + headers.get(it))
+			connection.setRequestProperty(it, headers.get(it))
+		}
+
+		connection.doOutput = true
+
+		if (method == "POST")
+		{
+			connection.setRequestMethod("POST")
+			def writer = new OutputStreamWriter(connection.outputStream)
+			writer.write(payload)
+			writer.flush()
+			writer.close()
+		}
+		else if (method == "GET")
+		{
+			connection.setRequestMethod("GET")
+		}
+
+		
+		connection.connect();
+		
+
+		log(DEBUG,  "END doRESTHTTPCall")
+
+		return connection
+
+	}
+
 	static void main(String[] args) {
-      extractExchangeDetail();
+
+
+		CICDUtil util = new CICDUtil();
+
+		def props = util.init();
+      
+      	def exchangeDetail = util.extractExchangeAssetDetail(props);
+
+      	def apiInstance = util.provisionAPIManager(props, exchangeDetail);
+
+
+
+
+
    	} 
 }
